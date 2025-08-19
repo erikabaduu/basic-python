17a09e37829130972539e5a5e3fb11af936d5a44
erikabaduu <erikabadu15@gmail.com>
2025-08-19 00:07:37 +0000

    Add files via upload

---
+++ tupulesanddictionaries.py
@@ -0,0 +1,8 @@
+students= {'Kwame': ('Biology' , 'Chemistry', 'Physics'), 'Ama': ('Math', 'English', 'Bio'), 'Kojo':('History','Geography', 'English')}
+#Print all the subjects Kwame is taking.
+print(students['Kwame'])
+#Add a new student Esi with subjects ("Math", "Chemistry")
+students['Esi']= 'Math, Chemistry'
+#Find and print the second subject of Ama.
+print(students['Ama'][1])
+print(students['Esi'])
